bb546e0100c098974d04b7e6c61c586761720f7b
amritesh <amriteshbhu438@gmail.com>
2026-06-02 15:56:28 +0530

    add navbar to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react"
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,17 +9,19 @@ export default function Home() {
         <section className="container mx-auto px-4 py-32">
           <div className="mx-auto max-w-4xl text-center">
             <h1 className="text-black font-bold text-6xl mb-6">
-              A beeter Way to track your job Application
+              A better Way to track your job Application
             </h1>
             <p className="text-muted-foreground mb-10 text-xl">
               Capture, Organize, and manage your job application at one place.
             </p>
             <div className="flex flex-col items-center gap-4">
-              <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                Start for free <ArrowRight className="ml-2" />
-              </Button>
+              <Link href="/sign-up">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                  Start for free <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p className="text-sm text-muted-foreground">
-                Free forever. no credit card required
+                Free forever. no credit card required.
               </p>
             </div>
           </div>
